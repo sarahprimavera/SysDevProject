b9a58e91d56f1dc485d6a7aff027eadf0fe8b700
sarahprimavera <sarahprimavera787@gmail.com>
2022-04-21 12:56:02 -0400

    finished configuration for mvc structure

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/TermProject');
+  // Site Name
+  define('SITENAME', '420-411-VA: Term Project Application');
+
+  // Database Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'termprojectdb');
+?>
