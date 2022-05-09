97a28b292a4901d1b06cea24f093b4c70f933e25
Mohamed  Vall <5335081-BigNig-Yeman@users.noreply.gitlab.com>
2022-05-08 22:10:07 -0400

    Adding changes that will hopefully allow me to merge the branches again

--- app/views/home_page.php
+++ app/views/home_page.php
@@ -3,17 +3,15 @@
 <head>
 	<meta charset="utf-8">
 	<title></title>
-	<link rel="stylesheet" href="mainpagecss.css">
+	<link rel="stylesheet" href="/SysDevProject/public/css/mainpagecss.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <link rel = "icon" href = 
-"https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Africa_%28orthographic_projection%29.svg/1200px-Africa_%28orthographic_projection%29.svg.png" 
+		"https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Africa_%28orthographic_projection%29.svg/1200px-Africa_%28orthographic_projection%29.svg.png" 
         type = "image/x-icon">
 </head>
 <body style="background-color:#00a459;">
@@ -127,85 +125,180 @@
 				
 			</script>
   			</div>
-		</nav>
-		
-		
-		<div class="row-1-1">
-		
-		<input id="center" name="search" type="text" placeholder="Search available Mauritanian Dish" >
-		</div>
-	
-		<div class="moveleft">
+
+</nav>
+
+
+<table class="table">
+<thead class="table-light">
+</thead>
+<tr>
+<th scope="col">Food</th>
+<th scope="col">Food Name</th>
+<th scope="col">Description</th>
+<th scope="col">Price</th>
+<th scrop="col" colspan="2" class="text-center"> Actions</th>
+</tr>
+<tbody>
+<?php 
+foreach($data['foods'] as $foods){
+	echo"<tr>";
+	echo "<td></td>";
+	echo"<td>$foods->foodName</td>";
+	echo"<td>$foods->description</td>";
+	echo"<td>$foods->price</td>";
+	echo"<td>
+	<button id='addToCart' name='addToCart' style='background-color:#000000;'><a href='/SysDevProject/Home/AddCart/$foods->foodId'>Add to cart </a></button>
+	</td>";
+	echo"</tr>";
+}
+
+?>
+</tbody>
+</table>
+<!-- 	
+		<div class="col-md-4 column productbox" >
+		<?php 
+				// foreach($data['foods'] as $foods){
+				// 	echo "<img src='https://www.cesarsway.com/wp-content/uploads/2019/09/AdobeStock_195276899-1024x681.jpeg.webp' class='img-responsive'>";
+				// 	echo "<div class='producttitle'>$foods->foodName </div>";
+				// 	echo "<div class='productprice'><div class='pull-right'>";
+				// 	echo "<a href='/SysDevProject/Home/AddCart/$foods->foodId' class='btn btn-danger btn-sm' role='button'>Add to cart</a>";
+				// 	echo "</div>";
+				// 	echo "<div class='pricetext'>$foods->price</div>";
+				// 	echo "<p><a href='#'> View</a></p>";
+				// 	//echo "</div>";
+				// }
+			?>
+		</div> -->
+
+
+		<!--<div>
+			<form method="POST">
+				<input type="text" id="searchBar" name="searchBar" placeholder="Search available Mauritanian Dish">
+				<button type="submit" id="searchButton" name="searchButton" class='btn btn-dark'> <a href="/SysDevProject/Home/searchProduct">Search </a> </button>  
+			</form>
+		</div>-->
+
+		<?php 
+		//<path d='M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z'/>
+		//<path d='M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z'/>
+		//<svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' fill='currentColor' class='bi bi-eye' viewBox='0 0 16 16'>
+		//</svg>
+               /* foreach($data['foods'] as $foods){
+                    echo"<tr>";
+                    //echo "<td>$foods->image</td>";
+                    echo"<td>$foods->foodName</td>";
+                    echo"<td>$foods->price</td>";
+                    echo"<td>
+					<p><a id='box' href='Home/viewProduct/$foods->foodId'> 
+			   		<svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' fill='currentColor' class='bi bi-eye' viewBox='0 0 16 16'>
+					<path d='M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z'/>
+					<path d='M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z'/>
+					</svg>
+					View</a></p>
+                    </td>";
+                    echo"<td>
+                    <form method='POST'>
+                    <button id='addToCart' name='addToCart' class='btn btn-dark'><a href='Home/AddCart/$foods->foodId'>Add to cart </a></button>
+                    </form>
+                    </td>";
+                    echo"</tr>";
+                }*/
+
+				//original front end code for home page
+				/*<div class="moveleft">
 		<div class="box-wrapper">
 		
-			<div class="row-2-3">
+			<div class="col-2-3">
 			<div class="newRow">
-			<div class="middle-row">
-            <div id="box1">
+			<div class="middle-column">
+            <div id="box5">
                <img src="food1.jpg" alt="" /> <hr style="width:100%">
                <h2>Dish #1</h2>
                <p>Whatever description about the food in this box here</p>
                <p>$20.00</p>
                <p><a id="box" href="#"> 
-			   <i class="fa fa-eye" aria-hidden="true"></i>
+			   <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+				<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+				<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+				</svg>
 				View</a></p>
             </div>
 			
-            <div id="box2">
+            <div id="box6">
                <img src="food2.jpg" alt="" /> <hr style="width:100%">
                <h2>Dish #2</h2>
                <p>Whatever description about the food in this box here</p>
                <p>$20.00</p>
                <p><a id="box" href="#"> 
-			   <i class="fa fa-eye" aria-hidden="true"></i>
+			   <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+				<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+				<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+				</svg>
 				View</a></p>
             </div>
-            <div id="box3">
+			
+            <div id="box7">
                <img src="food3.jpg" alt="" /> <hr style="width:100%">
                <h2>Dish #3</h2>
                <p>Whatever description about the food in this box here</p>
                <p>$20.00</p>
                <p><a id="box" href="#"> 
-			   <i class="fa fa-eye" aria-hidden="true"></i>
+			   <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+				<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+				<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+				</svg>
 				View</a></p>
             </div>
 			</div>
 			</div>
 			</div>
-			<div class="row-2-3">
+			<div class="col-2-3">
 			<div class="newRow">
-			<div class="middle-row">
-            <div id="box4">
+			<div class="middle-column">
+            <div id="box8">
                <img src="food1.jpg" alt="" /> <hr style="width:100%">
                <h2>Dish #4</h2>
                <p>Whatever description about the food in this box here</p>
                <p>$20.00</p>
                <p><a id="box" href="#"> 
-			   <i class="fa fa-eye" aria-hidden="true"></i>
+			   <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+				<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+				<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+				</svg>
 				View</a></p>
             </div>
-            <div id="box5">
+            <div id="box9">
                <img src="food2.jpg" alt="" /> <hr style="width:100%">
                <h2>Dish #5</h2>
                <p>Whatever description about the food in this box here</p>
                <p>$20.00</p>
                <p><a id="box" href="#"> 
-			   <i class="fa fa-eye" aria-hidden="true"></i>
+			   <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+				<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+				<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+				</svg>
 				View</a></p>
             </div>
-            <div id="box6">
+            <div id="box10">
                <img src="food3.jpg" alt="" /> <hr style="width:100%">
                <h2>Dish #6</h2>
                <p>Whatever description about the food in this box here</p>
                <p>$20.00</p>
                <p><a id="box" href="#"> 
-			   <i class="fa fa-eye" aria-hidden="true"></i>
+			   <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+				<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+				<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+				</svg>
 				View</a></p>
             </div>
 			</div>
 		 </div>
 		 </div>
 		 </div>
-		 </div>
+		 </div>*/
+        
+            ?>
 </body>
 </html>
